228d747ddb4d73132efdef9e1af4519842673bb1
Xay Voong <coder@ieants.cc>
2022-04-13 16:25:59 -0700

    basic logon, local json file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # versatilecardcreator
 versatile card creator, where users can create custom cards, including but not limited to MTG.
+
+# Engine
+
+Node.js, Next.js, Vercel
+
+
---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    reactStrictMode: true,
+    serverRuntimeConfig: {
+        secret: '0B85FF74-1A45-4D5F-83EE-406863BD6B689FA4FA86-ADC8-4A39-9E3A-BBC18A1706E77260E0D7-1B6D-4D19-915B-0D17A508E131'
+    },
+    publicRuntimeConfig: {
+        apiUrl: process.env.NODE_ENV === 'development'
+            ? 'http://deb9:3000/api' // development api
+            : 'http://deb9:3000/api' // production api
+    }
+}
